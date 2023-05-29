6a0b226209fd93b3200aab7dd2b2380870c45751
Dario M Lopez <130505982+DarioMLop@users.noreply.github.com>
2023-05-29 16:20:06 +0200

    Add files via upload

---
+++ Prog Sist Inf/EjDoWhileEspacioLatino/Ej1DoWhile.js
@@ -0,0 +1,7 @@
+/*Realiza un script que le pida al usuario que telee la letra A (en mayúscula) y mientras no lo haga se lo vuelva a pedir. Usa un bulce do..while y la ventana alert para los mensajes */
+let letra = "";
+do{
+    letra = prompt("Introduce la letra A").toUpperCase();
+    
+}while(letra !="A")
+alert("AAAAA")
