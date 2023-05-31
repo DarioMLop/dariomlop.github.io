88a841803905add130fd3460a8d710cba00f8de1
Dario M Lopez <130505982+DarioMLop@users.noreply.github.com>
2023-05-31 20:21:56 +0200

    Added som funcionality

--- Prog Sist Inf/Examen 05_06_2023/Ej minimo maximo en un array.js
+++ Prog Sist Inf/Examen 05_06_2023/Ej minimo maximo en un array.js
@@ -1,14 +1,28 @@
 
-let num = Number;
+let input = -1;
+let num = Number;   
 let array = [];
 
-while (num != 0){
-    num = parseInt(prompt("Introduce números y para salir pulsa 0"));
-    array.push(num);  /**Con el metodo push() agrego números a mi array. Para mi tiene sentido que sean negativos*/
+while (input != 0){  // Bucle para repetir el codigo mientras input sea diferente de 0
+    input = prompt(`Introduce tu serie de números.
+Pulsa cero para salir`);  // Le pido números al usuario con un prompt
+    num = parseFloat(input.replace(",", "."));  /** Con parseFloat cambio de tipo string  a tipo Number. 
+    El programa acepta números negativos y decimales. 
+    Si el usuario introduce decimales separados con coma lo transforma a decimales separados por punto*/
+    if(!isNaN(num) && typeof(num) === "number"){
+        array.push(num);    /** Si los números introducidos NO son NAN y 
+        el tipo de num es Number añade los números al array con el metodo push()*/
+    } else {
+        continue;
+    }
 }
 
-let minimo = Math.min(...array);
-let maximo = Math.max(...array);
-let arraySorted = array.sort(function (param1, param2){ return param1 - param2});
+let minimo = Math.min(...array);    //Con el metodo Math.min() saco el número menor introducido
+let maximo = Math.max(...array);    //Con el metodo Math.max() hallo el número mayor introducido
+let arraySorted = array.sort(function (param1, param2){return param1 - param2});    /** Ordeno el array 
+de números introducidos por el usuario*/
 
-alert(``)
+alert(`El número menor es: ${minimo}
+El número mayor es: ${maximo}
+Los números que has introducido ordenados de menor a mayor: ${arraySorted.join(", ")}`)  /** Muestro al usuario 
+el número menor y el número mayor y la lista ordenada de números introducidos*/
