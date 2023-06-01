8c6814c4d6f2cb5036102daaadf6c3183a4c8f70
Dario M Lopez <130505982+DarioMLop@users.noreply.github.com>
2023-06-01 16:22:37 +0200

    Added more comments

--- Prog Sist Inf/Examen 05_06_2023/Ej Intercambio de variables.js
+++ Prog Sist Inf/Examen 05_06_2023/Ej Intercambio de variables.js
@@ -1,10 +1,11 @@
 /*Intercambio de valor entre dos variables*/
 /* Con tiempo y aburrimento lo he hecho de cuatro. Seguro hay alguna forma más fácil.
-Ahora que lo pienso con arrays e indices sería más rápido */
-let num = 66, num2 = 22, num3 = 99, num4 = 44, temp = 0, temp2 = 0;
+Ahora que lo pienso con arrays e indices sería más rápido pero es trampa 
+--> TO DO: Pedir al usuario los números a intercambiar*/
+let num = 66, num2 = 22, num3 = 99, num4 = 44, temp = 0, temp2 = 0;  /* Inicializamos variables*/
 
 console.log(`Números en orden original: 
-1º: ${num}, 2º: ${num2}, 3º: ${num3}, 4º: ${num4}`);
+1º: ${num}, 2º: ${num2}, 3º: ${num3}, 4º: ${num4}`);  /*Muestro al usuario los valores en orden inicial*/
 
 temp = num;  // temp = 66
 temp2 = num4  //temp2 = 44
@@ -14,4 +15,4 @@ num3 = temp;    // num3 = 66
 num2 = temp2;   // num2 = 44
 
 console.log(`Magia. Valores intercambiados (pares con pares): 
-1º: ${num}, 2º: ${num2}, 3º: ${num3}, 4º: ${num4}`);
+1º: ${num}, 2º: ${num2}, 3º: ${num3}, 4º: ${num4}`);    /*Muestro al usuario los valores cambiados*/
