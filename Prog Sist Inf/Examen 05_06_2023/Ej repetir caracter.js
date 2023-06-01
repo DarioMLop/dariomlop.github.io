1ac53d1463e515cb01dd132f3e635ae723618a80
Dario M Lopez <130505982+DarioMLop@users.noreply.github.com>
2023-06-01 15:36:42 +0200

    Update code

--- Prog Sist Inf/Examen 05_06_2023/Ej repetir caracter.js
+++ Prog Sist Inf/Examen 05_06_2023/Ej repetir caracter.js
@@ -4,10 +4,15 @@ Usar un bucle do..while para crear una cadena que contenga letras repetidas. Las
 Si elijo X y luego telco 10 se mostrará XXXXXXXXXX. Si tecleo z me olverá a pedir letra, y sitecelo 20 me volverá a pedir el número
 */
 
-let character = prompt("Introduce el caracter. Z para terminar").toUpperCase();
-let repeticiones = parseInt(Math.abs(prompt("Introduce el número de repeticiones. Introduce 0 para salir")))
+let character = "", char = "", repeticiones = 0;    /* Inicializo tres variables*/
 
-do {
-    alert(character.repeat(repeticiones))
-    repeticiones--
-} while(character !== "Z" || repeticiones !== 0)
+do {    // Creo un bucle do...while 
+    character = prompt("Introduce X o Z");  /* Creo un prompt para pedir el caracter al usuario*/
+    char = character.toUpperCase();  /* Con el método toUpperCase() me aseguro que el caracter esta en el formato deseado*/
+} while(char != "Z" && char != "X")  /*El bucle continuara mientras la letra introducida no sea la z o la x*/
+
+do {    /*Creo un segundo bucle do...while */
+    repeticiones = parseInt(prompt("Introduce el número de repeticiones"))  /* Dentro pido al usuario cuantas repeticiones desea*/
+} while(repeticiones < 1 || repeticiones > 15)   /* El bucle continua pidiendo el número hasta que esté entre el rango 1 y 15*/
+    
+alert(`${char.repeat(repeticiones)}`)   /* Por ultimo le muestro al usuario el caracter repetido gracias al metodo repeat()*/
